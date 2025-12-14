b24d9a0667aec4f48084049a90ccabebf7cb33d4
kdn0325 <dn10003@gmail.com>
2025-12-15 00:50:22 +0900

    chore: update babel configuration to support testing and add @react-native/babel-preset dependency

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
-module.exports = {
-  presets: [
-    'module:react-native-builder-bob/babel-preset',
-    '@babel/preset-flow',
-  ],
+module.exports = function (api) {
+  const isTest = api.env('test');
+  api.cache(true);
+
+  return {
+    presets: isTest
+      ? [['@react-native/babel-preset', { disableImportExportTransform: true }]]
+      : [
+          ['@babel/preset-flow', { all: true }],
+          'module:react-native-builder-bob/babel-preset',
+        ],
+  };
 };
