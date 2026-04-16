a6d5620f18f1015af63629cd0aacfaa367cf83cb
Flikest <grecmanviktor6@gmail.com>
2026-04-16 20:42:37 +0600

    feat: custom colors and response country id on tap

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,29 @@
-import { WorldMap } from 'react-native-simple-worldmap';
+import { allCountries, WorldMap } from 'pingvi-simple-worldmap';
 import { View, StyleSheet } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [selectedCountries, setSelectedCountries] = useState<string[]>([]);
+
+  console.log(allCountries);
+
+  const handleSelect = (countries: string[]) => {
+    setSelectedCountries(countries);
+    console.log(selectedCountries);
+  };
+
   return (
     <View style={styles.container}>
-      <WorldMap />
+      <WorldMap
+        color="#8b2ab1"
+        isSelectable={true}
+        customColors={{
+          RU: '#15ff00',
+          US: '#15ff00',
+        }}
+        selectedColor={'#ff0303'}
+        onSelect={handleSelect}
+      />
     </View>
   );
 }
